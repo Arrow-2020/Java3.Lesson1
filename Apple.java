cd0a4393cd851f37ec6c427e721359347fa0e8c0
Вячеслав <70411183+Arrow-2020@users.noreply.github.com>
2021-11-20 15:39:36 +0300

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,8 @@
+package 3level.lesson1;
+
+public class Apple extends 3level.lesson1.Fruit {
+    @Override
+    float getWeight() {
+        return 1.0f;
+    }
+}
---
+++ Fruit.java
@@ -0,0 +1,5 @@
+package 3level.lesson1;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
